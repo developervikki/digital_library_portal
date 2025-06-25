2dcd0e79c9469e2c578bb74f384f9426d1dd3887
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2025-06-25 13:21:43 +0530

    Update book-seat.php

--- student/book-seat.php
+++ student/book-seat.php
@@ -6,7 +6,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-if (!isset($_SESSION['student_id'])) {
+if (!isset($_SESSION['student_id'])) { 
     header("Location: ../login.php");
     exit();
 }
