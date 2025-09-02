cb2f40727414a95675e0c08c8028ab4c2be4556f
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2025-09-02 17:41:31 +0530

    Update dashboard.php

--- student/dashboard.php
+++ student/dashboard.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-if (!isset($_SESSION['student_id']))  {
+if (!isset($_SESSION['student_id']))  
   header("Location: ../login.php"); 
-  exit(); 
+  exit();  
 }
 
 include '../includes/db.php';
