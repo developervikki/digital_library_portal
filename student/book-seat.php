f4dc2947861f638bf07594fe315f84ab95a5c9f6
Vikash Yadav <91186160+developervikki@users.noreply.github.com>
2025-10-06 16:37:37 +0530

    Update book-seat.php

--- student/book-seat.php
+++ student/book-seat.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../includes/db.php';
-require_once '../includes/auth.php'; 
+require_once '../includes/auth.php';  
   
 if (session_status() === PHP_SESSION_NONE) { 
     session_start();  
